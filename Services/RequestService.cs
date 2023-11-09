4153ba2a4a83af4205b489deaa3bb2352699d737
Mystic Quest <kirimns@gmail.com>
2023-11-09 14:46:30 +0200

    uses new httpClientFactory instead

--- Services/RequestService.cs
+++ Services/RequestService.cs
@@ -1,33 +1,40 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Services
 {
     public class RequestService
     {
-        private readonly HttpClient _httpClient;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<RequestService> _logger;
 
-        public RequestService(HttpClient httpClient)
+        public RequestService(IHttpClientFactory httpClientFactory, ILogger<RequestService> logger)
         {
-            _httpClient = httpClient;
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public async Task<string> MakeRequestAsync(string requestUri)
+        public async Task<string> MakeRequestAsync(string requestUri, CancellationToken cancellationToken = default)
         {
+            if (string.IsNullOrWhiteSpace(requestUri))
+                throw new ArgumentException("Request URI cannot be null or whitespace.", nameof(requestUri));
+
+            var httpClient = _httpClientFactory.CreateClient();
+
             try
             {
-                HttpResponseMessage response = await _httpClient.GetAsync(requestUri);
+                var response = await httpClient.GetAsync(requestUri, cancellationToken);
                 response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
+                return await response.Content.ReadAsStringAsync(cancellationToken);
             }
             catch (HttpRequestException e)
             {
-                // Log error (we'll just print to console for now)
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-                return null;
+                _logger.LogError(e, "HttpRequestException caught while making a request to {RequestUri}", requestUri);
+                // keeps stack trace
+                throw;
             }
         }
     }
