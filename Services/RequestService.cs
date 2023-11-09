b86731c8845836ca287faac64c6f7f45898b591a
Mystic Quest <kirimns@gmail.com>
2023-11-09 14:38:09 +0200

    adds request test

---
+++ Services/RequestService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class RequestService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RequestService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> MakeRequestAsync(string requestUri)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(requestUri);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                // Log error (we'll just print to console for now)
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return null;
+            }
+        }
+    }
+}
