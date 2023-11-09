072d129f4c487c434530da8ee59ded653dcd4b8d
Mystic Quest <kirimns@gmail.com>
2023-11-09 21:19:37 +0200

    adds controller to expose api operations

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace DataAccess
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
     }
 }
---
+++ Services/ItemsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using Models;
+
+namespace Services.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+
+        public ItemsController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllItems()
+        {
+            var items = await _itemService.GetAllItemsAsync();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItem(int id)
+        {
+            var item = await _itemService.GetItemByIdAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItem([FromBody] Item item)
+        {
+            await _itemService.CreateItemAsync(item);
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItem(int id, [FromBody] Item item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            await _itemService.UpdateItemAsync(item);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            await _itemService.DeleteItemAsync(id);
+            return NoContent();
+        }
+    }
+}
