3bfe6542e196b4dc9408b6e9e53471be16f98ac7
Mystic Quest <kirimns@gmail.com>
2023-11-10 16:38:24 +0200

    adds a new model

---
+++ Models/PingData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class PingData
+    {
+        public int Id { get; set; }
+        public string Domain { get; set; }
+        public long RoundtripTime { get; set; }
+        public string Status { get; set; }
+    }
+}
