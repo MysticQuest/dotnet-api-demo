15c60448ca930278902156e4bbbe87826e91bd5a
Mystic Quest <kirimns@gmail.com>
2023-11-10 02:14:06 +0200

    changes datetime to string

--- Models/Item.cs
+++ Models/Item.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Url { get; set; }
-        public DateTime DateTime { get; set; }
+        public string DateTime { get; set; }
         public string HttpCode { get; set; }
     }
 }
