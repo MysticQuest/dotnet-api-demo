d6e497f2711efee3bf9b44124451d109279f2c55
Mystic Quest <kirimns@gmail.com>
2023-11-10 20:44:22 +0200

    fixes paths

--- Views/ApiClient.cs
+++ Views/ApiClient.cs
@@ -20,33 +20,33 @@ namespace Views
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            var response = await _httpClient.GetAsync($"api/Generic?typeName={_typeName}");
+            var response = await _httpClient.GetAsync($"Generic?typeName={_typeName}");
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadFromJsonAsync<IEnumerable<T>>();
         }
 
         public async Task<T> GetByIdAsync(int id)
         {
-            var response = await _httpClient.GetAsync($"api/Generic/{id}?typeName={_typeName}");
+            var response = await _httpClient.GetAsync($"Generic/{id}?typeName={_typeName}");
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadFromJsonAsync<T>();
         }
 
         public async Task TriggerCreateAsync()
         {
-            var response = await _httpClient.PostAsJsonAsync($"api/Generic?typeName={_typeName}", new { });
+            var response = await _httpClient.PostAsJsonAsync($"Generic?typeName={_typeName}", new { });
             response.EnsureSuccessStatusCode();
         }
 
         public async Task UpdateAsync(int id, T item)
         {
-            var response = await _httpClient.PutAsJsonAsync($"api/Generic/{id}?typeName={_typeName}", item);
+            var response = await _httpClient.PutAsJsonAsync($"Generic/{id}?typeName={_typeName}", item);
             response.EnsureSuccessStatusCode();
         }
 
         public async Task DeleteAsync(int id)
         {
-            var response = await _httpClient.DeleteAsync($"api/Generic/{id}?typeName={_typeName}");
+            var response = await _httpClient.DeleteAsync($"Generic/{id}?typeName={_typeName}");
             response.EnsureSuccessStatusCode();
         }
     }
