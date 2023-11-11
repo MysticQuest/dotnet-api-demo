509a80fd39c8bcc583300c2c7e37400d5ed9a409
Mystic Quest <kirimns@gmail.com>
2023-11-11 02:30:58 +0200

    adds error catching to controller

--- Services/GenericController.cs
+++ Services/GenericController.cs
@@ -19,50 +19,105 @@ namespace Services
         [HttpGet]
         public async Task<IActionResult> GetAll([FromQuery] string typeName)
         {
-            dynamic service = GetServiceForType(typeName);
-            var items = await service.GetAllAsync();
-            return Ok(items);
+            try
+            {
+                dynamic service = GetServiceForType(typeName);
+                var items = await service.GetAllAsync();
+                return Ok(items);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred in GetAll method.");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while retrieving items.");
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> Get([FromQuery] string typeName, int id)
         {
-            dynamic service = GetServiceForType(typeName);
-            var item = await service.GetByIdAsync(id);
-            if (item == null)
+            try
             {
-                return NotFound();
+                dynamic service = GetServiceForType(typeName);
+                var item = await service.GetByIdAsync(id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                return Ok(item);
             }
-            return Ok(item);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred in Get method.");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while retrieving the item.");
+            }
         }
 
         [HttpPost]
         public async Task<IActionResult> Create([FromQuery] string typeName)
         {
-            dynamic service = GetServiceForType(typeName);
-            var createdItem = await service.CreateAsync();
-            return CreatedAtAction(nameof(Get), new { typeName = typeName, id = createdItem.Id }, createdItem);
+            try
+            {
+                dynamic service = GetServiceForType(typeName);
+                var createdItem = await service.CreateAsync();
+                if (createdItem == null)
+                {
+                    _logger.LogWarning("Created item was null for type {TypeName}.", typeName);
+                    return NoContent();
+                }
+                return CreatedAtAction(nameof(Get), new { typeName = typeName, id = createdItem.Id }, createdItem);
+            }
+            catch (ArgumentException ex)
+            {
+                _logger.LogError(ex, "ArgumentException in Create method for type {TypeName}.", typeName);
+                return BadRequest(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.LogError(ex, "InvalidOperationException in Create method for type {TypeName}.", typeName);
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected error occurred in Create method for type {TypeName}.", typeName);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> Update([FromQuery] string typeName, int id, [FromBody] dynamic entity)
         {
-            dynamic service = GetServiceForType(typeName);
-            if (entity.Id != id)
+            try
             {
-                return BadRequest();
-            }
+                dynamic service = GetServiceForType(typeName);
+                if (entity.Id != id)
+                {
+                    return BadRequest("The ID in the body does not match the ID in the path.");
+                }
 
-            await service.UpdateAsync(entity);
-            return NoContent();
+                await service.UpdateAsync(entity);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred in Update method.");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while updating the item.");
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete([FromQuery] string typeName, int id)
         {
-            dynamic service = GetServiceForType(typeName);
-            await service.DeleteAsync(id);
-            return NoContent();
+            try
+            {
+                dynamic service = GetServiceForType(typeName);
+                await service.DeleteAsync(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred in Delete method.");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while deleting the item.");
+            }
         }
 
         private dynamic GetServiceForType(string typeName)
